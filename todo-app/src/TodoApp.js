4c4b0e361a8cb3295cadf1bc3c3005410714436f
jshields123 <jackshields@live.co.uk>
2021-09-12 16:22:36 +0930

    added in handle change

--- todo-app/src/TodoApp.js
+++ todo-app/src/TodoApp.js
@@ -5,16 +5,23 @@ export default class TodoApp extends React.Component{
     constructor(props) {
         super(props)
         this.state = { items: [], text: ''}
+        // this.handleChange = this.handleChange.bind(this)
+        // this.handleSubmit = this.handleSubmit.bind(this)
     }
+
     render(){
         return(
             <div>
                 <h3>Todo App</h3>
-                <form >
+                <form onSubmit={this.handleSubmit}>
                     <label htmlFor="new-todo">
                         New task to do? 
                     </label>
-                    <input id="new-todo" />
+                    <input 
+                    id="new-todo"
+                    onChange={this.handleChange}
+                    value={this.state.text}
+                     />
                     <button>
                         Add #{this.state.items.length + 1}
                     </button>
@@ -22,7 +29,29 @@ export default class TodoApp extends React.Component{
             </div>
         )
     }
-}
+
+        handleChange(e){
+            this.setState({ text: e.target.value })
+        }
+
+        handleSubmit(e){
+            e.preventDefault()
+            if (this.state.text.length === 0){
+                return
+            }
+            const newItem = {
+                text: this.state.text,
+                id: Date.now()
+            }
+            this.setState(state => ({
+                items: this.state.items.concat(newItem),
+                text: ''
+            }))
+        }
+    }
+
+    
+
 
 
 ReactDOM.render(<TodoApp />, document.getElementById('root'))
