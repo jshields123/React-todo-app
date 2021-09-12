6be65bbb4d4e01f123995ee7f841b0fc7b47f1c4
jshields123 <jackshields@live.co.uk>
2021-09-12 15:31:28 +0930

    added in form and labels for inputting tasks

--- todo-app/src/TodoApp.js
+++ todo-app/src/TodoApp.js
@@ -2,11 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 export default class TodoApp extends React.Component{
- 
+    constructor(props) {
+        super(props)
+        this.state = { items: [], text: ''}
+    }
     render(){
         return(
             <div>
                 <h3>Todo App</h3>
+                <form >
+                    <label htmlFor="new-todo">
+                        New task to do? 
+                    </label>
+                    <input id="new-todo" />
+                    <button>
+                        Add #{this.state.items.length + 1}
+                    </button>
+                </form>
             </div>
         )
     }
